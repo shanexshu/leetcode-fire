0ec87a7ecd03a25dbcd7b4a5c34f9d5bb1256aa1
Shane Shu <shuxin0@gmail.com>
2021-04-05 14:38:32 -0500

    Time: 168 ms (47.71%), Space: 20.7 MB (28.51%)

---
+++ search_a_2d_matrix_ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        m, n = len(matrix), len(matrix[0])
+        
+        row = m-1
+        col = 0
+        
+        while row>=0 and col<n:
+            if matrix[row][col] == target: return True
+            if matrix[row][col]>target:
+                row -= 1
+            else:
+                col += 1
+        
+        return False
