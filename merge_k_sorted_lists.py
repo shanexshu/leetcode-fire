29f5a7d7524e347479fdbdf624071973fc54aa9c
Shane Shu <shuxin0@gmail.com>
2021-07-07 21:11:34 -0500

    Time: 112 ms (59.24%), Space: 17.8 MB (64.38%)

--- merge_k_sorted_lists.py
+++ merge_k_sorted_lists.py
@@ -6,25 +6,26 @@
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
         
-        def merge(l1, l2):
-            if not l1: return l2
-            if not l2: return l1
-            if l1.val > l2.val:
-                return merge(l2, l1)
-            # l1 has lesser first val
-            ret = ListNode(l1.val)
-            ret.next = merge(l1.next, l2)
-            return ret
-            
-        if not lists: return None
-        if len(lists)<2: return lists[0]
+        if len(lists)==0:
+            return None
+        if len(lists)==1:
+            return lists[0]
         if len(lists)==2:
-            h1 = lists[0]
-            h2 = lists[1]
-            # merge the two lists
-            return merge(h1, h2)
-        
+            return self.mergeTwo(lists[0], lists[1])
         mid = len(lists)//2
-        return merge(self.mergeKLists(lists[:mid]), self.mergeKLists(lists[mid:]))
-    
-    
+        return self.mergeTwo(self.mergeKLists(lists[:mid]), self.mergeKLists(lists[mid:]))
+              
+    def mergeTwo(self, l1, l2):
+        ans = ListNode()
+        node = ans
+        while l1 and l2:
+            if l1.val<=l2.val:
+                node.next = l1
+                l1 = l1.next
+            else:
+                node.next = l2
+                l2 = l2.next
+            node = node.next
+        if l1: node.next = l1
+        if l2: node.next = l2
+        return ans.next
