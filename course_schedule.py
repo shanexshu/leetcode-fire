352e68749eda6f0e3e271ef6a5d799c9a59b00f7
Shane Shu <shuxin0@gmail.com>
2021-07-11 16:21:00 -0500

    Time: 88 ms (97.44%), Space: 15.6 MB (71.94%)

--- course_schedule.py
+++ course_schedule.py
@@ -1,22 +1,25 @@
+from collections import deque
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        todo = deque()
-        post = [[] for _ in range(numCourses)]
-        prereqs = [0]*numCourses
-        for c, pre in prerequisites:
-            post[pre].append(c)
-            prereqs[c] += 1
+        adj = [set() for _ in range(numCourses)]
+        prereqs = [0]* numCourses
+        for a, b in prerequisites:
+            adj[b].add(a)
+            prereqs[a] += 1
         
+        q = deque()
         for i in range(numCourses):
             if prereqs[i]==0:
-                todo.append(i)
-        ans = 0
-        while todo:
-            course = todo.popleft()
-            ans += 1
-            for nc in post[course]:
-                prereqs[nc] -= 1
-                if prereqs[nc] == 0:
-                    todo.append(nc)
+                q.append(i)
         
-        return ans==numCourses
+        taken = set()
+        while q:
+            # this should be only courses that have no prereqs left
+            course = q.popleft()
+            taken.add(course)
+            for post in adj[course]:
+                prereqs[post] -= 1
+                if prereqs[post]==0:
+                    q.append(post)
+        
+        return len(taken)==numCourses
