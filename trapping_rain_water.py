0db560ba7dffd447a38b4e46a8fab1e174f4fe55
Shane Shu <shuxin0@gmail.com>
2021-07-14 12:24:21 -0500

    Time: 52 ms (82.10%), Space: 14.9 MB (36.86%)

--- trapping_rain_water.py
+++ trapping_rain_water.py
@@ -2,13 +2,17 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         n = len(height)
         if n==0: return 0
-        dpleft = [height[0]]*n
-        dpright = [height[-1]]*n
+        left_max = [height[0]]*n
+        right_max = [height[-1]]*n
         ans = 0
+        
         for i in range(1, n):
-            dpleft[i] = max(height[i], dpleft[i-1])
-        for i in range(n-2,-1,-1):
-            dpright[i] = max(height[i], dpright[i+1])
+            left_max[i] = max(height[i], left_max[i-1])
+        
+        for i in reversed(range(0, n-1)):
+            right_max[i] = max(height[i], right_max[i+1])
+        
         for i in range(n):
-            ans += min(dpleft[i], dpright[i]) - height[i]
+            ans += min(left_max[i], right_max[i]) - height[i]
+        
         return ans
