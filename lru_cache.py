5d34d3679894b043f8c39ab8503bd9b0606f012d
Shane Shu <shuxin0@gmail.com>
2021-07-19 16:24:38 -0700

    Time: 784 ms (47.34%), Space: 74.8 MB (5.94%)

--- lru_cache.py
+++ lru_cache.py
@@ -1,62 +1,28 @@
-class ListNode:
-    def __init__(self, val=0, key=0):
-        # nodes will hold the value of the key, as well as the key
-        self.val = val
-        self.next = None
-        self.prev = None
-        self.key = key
-
 class LRUCache:
-
-    def __init__(self, capacity: int):
-        # dictionary with (key, nodeptr)
-        self.dic = {}
+    
+    def __init__(self, capacity):
         self.cap = capacity
-        
-        # dummy head and tail nodes
-        self.head = ListNode(-1, -1)
-        self.tail = ListNode(-1, -1)
-        self.head.next = self.tail
-        self.tail.prev = self.head
+        self.dic = OrderedDict()
 
-    def remove(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
-        
-    def add(self, node):
-        node.next = self.head.next
-        node.prev = self.head
-        
-        self.head.next.prev = node
-        self.head.next = node
-        
-    def get(self, key: int) -> int:
+    def get(self, key):
+        # Return the value of the key if the key exists, otherwise -1.
         if key in self.dic:
-            node = self.dic[key]
-            # put it at front
-            self.remove(node)
-            self.add(node)
-            
-            return node.val
+            self.dic.move_to_end(key, last=False)
+            return self.dic[key]
         return -1
 
-    def put(self, key: int, value: int) -> None:
-        newnode = ListNode(value, key)
-        if key in self.dic:
-            # remove it
-            node = self.dic[key]
-            self.remove(node)
-        
-        self.dic[key] = newnode
-            
-        # put new at front
-        self.add(newnode)
-            
-        # if at cap, evict the tail
+    def put(self, key, value):
+        # Update the value of the key if the key exists. 
+        # Otherwise, add the key-value pair to the cache. 
+        # If the number of keys exceeds the capacity from this operation, 
+        # evict the least recently used key.
+
+        self.dic[key] = value
+        self.dic.move_to_end(key, last=False)
         if len(self.dic)>self.cap:
-            node = self.tail.prev
-            self.remove(node)
-            del self.dic[node.key]
+            self.dic.popitem(last=True)
+
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
